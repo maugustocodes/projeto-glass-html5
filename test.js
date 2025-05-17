d4a020687e49f333a315fbdc13d2a886d00f7eec
maugustocodes <marcelo.swimming@gmail.com>
2025-05-17 16:52:47 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang=>
+<head>  
+    <title>Teste JavaScript</title>
+    <meta charset="UTF-8"/>
+</head>
+<body>
+    <h1>Acenda a Lâmpada</h1>
+    <img src="_im"
+</body>
+</html>
